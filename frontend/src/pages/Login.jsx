5f1b149ac8d915a588310ed68d96604d13493800
keerthanapugalenthi <keerthanapugalenthi@gmail.com>
2026-07-24 21:32:33 +0530

    Build login page UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,48 @@
+import "./../styles/Login.css";
+
 function Login() {
-  return <h1>Login Page</h1>;
+  return (
+    <div className="login-page">
+      <div className="login-card">
+
+        <h1 className="logo">DustNet AI</h1>
+
+        <p className="subtitle">
+          Industrial Rover Management Platform
+        </p>
+
+        <form>
+
+          <div className="input-group">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <div className="input-group">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <button className="login-btn">
+            Login
+          </button>
+
+        </form>
+
+        <p className="register-text">
+          Don't have an account?
+          <span> Register</span>
+        </p>
+
+      </div>
+    </div>
+  );
 }
 
 export default Login;
